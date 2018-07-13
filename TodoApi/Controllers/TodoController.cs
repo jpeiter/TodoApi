04d98084c210f5f9eddb7fca788d07219f70a12d
Jean Felipe Peiter <jpeiter15@gmail.com>
2018-07-12 21:36:48 -0300

    Add project files.

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/Todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext context;
+
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+
+            if (this.context.TodoItems.Count() == 0)
+            {
+                this.context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                this.context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return this.context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(long id)
+        {
+            var item = this.context.TodoItems.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem item)
+        {
+            this.context.TodoItems.Add(item);
+            this.context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, TodoItem item)
+        {
+            var todo = this.context.TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            this.context.TodoItems.Update(todo);
+            this.context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+
+            var todo = this.context.TodoItems.Find(id);
+
+            if(todo == null)
+            {
+                return NotFound();
+            }
+
+            this.context.TodoItems.Remove(todo);
+            this.context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
